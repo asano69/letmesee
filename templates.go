7b2a27870ed42b109ae694791cbeaf3f6c850da6
asano69 <186772504+asano69@users.noreply.github.com>
2026-04-15 04:32:08 +0900

    indexページが表示されないものの正常に機能

--- templates.go
+++ templates.go
@@ -20,8 +20,7 @@ import (
 const staticTemplatesDir = "static/templates"
 
 // tmplHeader is the page header, equivalent to skel/header.rhtml.
-const tmplHeader = `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-        "http://www.w3.org/TR/html4/strict.dtd">
+const tmplHeader = `<!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
